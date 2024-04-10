8d27b156a1b239768db7072233bdfc8dd1c6ddc3
silimechok <theerawat.silimechok@accenture.com>
2024-04-10 18:57:31 +0700

    AddStateANdLOgin

---
+++ src/components/Login.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components"
+
+export default function Login() {
+    return <Container>login</Container>
+    
+}
+
+const Container = styled.div``;
